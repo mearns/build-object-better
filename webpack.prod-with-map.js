fe240e3085d0963fc1b2405f65d0c76b0af8c857
Brian Mearns <mearns.b@gmail.com>
2018-11-13 15:46:45 -0500

    Removing UMD stuff, it's just bloating the package
    Will attempt to publish an alternate package for the UMD stuff.

--- webpack.prod-with-map.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-const commonProd = require('./webpack-config/webpack.prod.common.js')
-
-module.exports = merge(commonProd, {
-  entry: {
-    'min-with-map': './index.js'
-  },
-  devtool: 'source-map'
-})
--- webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge')
-const commonProd = require('./webpack-config/webpack.prod.common.js')
-
-module.exports = merge(commonProd, {
-  entry: {
-    min: './index.js'
-  }
-})
