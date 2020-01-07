48ec17837e7b498edbc8b74b99b839f9ed02a906
Brian Mearns <mearns.b@gmail.com>
2020-01-06 20:59:10 -0500

    Remove unused dependencies

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,11 @@
 /* eslint-env mocha */
 /* eslint @typescript-eslint/no-var-requires: 0 */
 
+/**
+ * This is a quick sanity check to make sure that the compiled main-entry point
+ * works. It requires that the code has been compiled.
+ */
+
 const bob = require("..");
 
 const { expect } = require("chai");
